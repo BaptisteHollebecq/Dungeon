43c1d0dbd68026e8114b1b76ebbb7751b05833e2
BaptisteHollebecq <baptiste.hollebecq@pole3d.com>
2020-04-07 09:14:19 +0200

    Update Room.cs

--- Dungeon/Dungeon/Room.cs
+++ Dungeon/Dungeon/Room.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Dungeon
 {
+    enum Direction { N, S, E, W };
+
     class Room
     {
+        public int Index;
+
+        public Items Loot;
+        public Enemy Fight;
+        public bool Exit;
+        public bool Bed;
+        public List<Room> SideRooms = new List<Room>();
+        
+    }
+
+    class Dungeon : Room
+    {
+        public List<Room> Rooms = new List<Room>();
+        public int PlayerIndex;
+
+        public Dungeon()
+        {
+
+        }
+
     }
 }
